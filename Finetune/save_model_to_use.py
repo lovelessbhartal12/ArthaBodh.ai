77e44bc021268ca35d65682ec0a0f996edcd4d93
Roshan Acharya <roshanacharya9808@gmail.com>
2026-02-07 22:51:59 +0545

    feat: Add python file to save model

---
+++ Finetune/save_model_to_use.py
@@ -0,0 +1,39 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+
+BASE_MODEL = "Qwen/Qwen3-1.7B"
+LORA_PATH = "./qwen_nepali_budget_lora"
+MERGED_MODEL_PATH = "../RAG/qwen_nepali_budget_merged"
+
+# -----------------------
+# Load tokenizer
+# -----------------------
+tokenizer = AutoTokenizer.from_pretrained(LORA_PATH, trust_remote_code=True)
+
+# -----------------------
+# Load base model
+# -----------------------
+model = AutoModelForCausalLM.from_pretrained(
+    BASE_MODEL,
+    device_map="auto",
+    torch_dtype=torch.float16,
+    trust_remote_code=True
+)
+
+# -----------------------
+# Load LoRA adapters
+# -----------------------
+model = PeftModel.from_pretrained(model, LORA_PATH)
+
+def save():
+    model = model.merge_and_unload()
+    model.save_pretrained(MERGED_MODEL_PATH)
+    tokenizer.save_pretrained(MERGED_MODEL_PATH)
+
+
+
+if __name__ == "__main__":
+    save()
+
+
