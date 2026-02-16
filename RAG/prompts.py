f9de3ed88b482b2801e9926c6b5e59336c536691
Roshan Acharya <roshanacharya9808@gmail.com>
2026-02-16 22:26:19 +0545

    all changes

--- RAG/prompts.py
+++ RAG/prompts.py
@@ -3,11 +3,14 @@ from langchain_core.prompts import PromptTemplate
 def budget_prompt():
     return PromptTemplate(
         template="""
-Instruction: माथिको विवरण अनुसार
- {input}
-Input: {instruction}
+### Instruction: तपाईं नेपाल सरकारको बजेट सम्बन्धी जानकारी दिने सहायक हुनुहुन्छ।
+तल दिइएको सन्दर्भको आधारमा प्रयोगकर्ताको प्रश्नको स्पष्ट र तथ्यमा आधारित उत्तर दिनुहोस्।
 
-Response:
+
+ 
+### Context: {input} {instruction}
+
+### Response:
 """,
         input_variables=["instruction", "input"]
     )
