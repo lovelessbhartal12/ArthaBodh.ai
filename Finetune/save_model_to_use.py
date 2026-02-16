77254e90c6d80aef4df15c9f8dff85e28bbe8093
Roshan Acharya <roshanacharya9808@gmail.com>
2026-02-16 22:26:55 +0545

    feat:add save model option

--- Finetune/save_model_to_use.py
+++ Finetune/save_model_to_use.py
@@ -8,25 +8,24 @@ MERGED_MODEL_PATH = "../RAG/qwen_nepali_budget_merged"
 
 # -----------------------
 # Load tokenizer
-# -----------------------
-tokenizer = AutoTokenizer.from_pretrained(LORA_PATH, trust_remote_code=True)
 
-# -----------------------
-# Load base model
-# -----------------------
-model = AutoModelForCausalLM.from_pretrained(
-    BASE_MODEL,
-    device_map="auto",
-    torch_dtype=torch.float16,
-    trust_remote_code=True
-)
-
-# -----------------------
-# Load LoRA adapters
-# -----------------------
-model = PeftModel.from_pretrained(model, LORA_PATH)
 
 def save():
+    #load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(LORA_PATH, trust_remote_code=True)
+
+    # Load base model
+    model = AutoModelForCausalLM.from_pretrained(
+        BASE_MODEL,
+        device_map="auto",
+        torch_dtype=torch.float16,
+        trust_remote_code=True
+    )
+
+   
+    # Load LoRA adapters
+
+    model = PeftModel.from_pretrained(model, LORA_PATH)
     model = model.merge_and_unload()
     model.save_pretrained(MERGED_MODEL_PATH)
     tokenizer.save_pretrained(MERGED_MODEL_PATH)
